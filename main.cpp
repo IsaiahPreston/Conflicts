04f9d2de60fc681b28716c3577a31b1ed32b7fe4
Isaiah PrestonE <ijpreston@nic.edu>
2025-03-26 13:49:17 -0700

    addes say function:

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void say(string s);
+
 int main() {
     cout << "Greeting . . . " << endl;
     cout << "Hello world!" << endl;
     return 0;
+}
+
+void say(string s) {
+    cout << s << endl;
 }
