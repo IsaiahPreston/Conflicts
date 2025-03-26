74979908386551145b7fff8c6518d94558330b2d
Isaiah PrestonE <ijpreston@nic.edu>
2025-03-26 14:08:50 -0700

    Sending restored code to hub

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 void say(string s);
+int add (int a, int b);
 
 int main() {
-
+    cout << "Greeting . . . " << endl;
+    cout << "Hello world!" << endl;
     return 0;
 }
 
 void say(string s) {
     cout << s << endl;
+}
+int add (int a, int b) {
+    
+    return a + b;
 }
