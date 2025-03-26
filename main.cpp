a19114e50e9c89fd479bf71cd05bc55b301036fc
IsaiahPreston <ijpreston@nic.edu>
2025-03-26 20:42:37 +0000

    added add funtion;

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int add (int a, int b);
+
 int main() {
     cout << "Hello world!" << endl;
     return 0;
+}
+
+int add (int a, int b) {
+    return a + b;
 }
