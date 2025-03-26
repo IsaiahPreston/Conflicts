6df2cfe01d44cd791fbe519233a74f4f637b0d47
Isaiah PrestonE <ijpreston@nic.edu>
2025-03-26 14:01:24 -0700

    mistake in repository

--- main.cpp
+++ main.cpp
@@ -2,18 +2,12 @@
 using namespace std;
 
 void say(string s);
-int add (int a, int b);
 
 int main() {
-    cout << "Greeting . . . " << endl;
-    cout << "Hello world!" << endl;
+
     return 0;
 }
 
 void say(string s) {
     cout << s << endl;
-}
-int add (int a, int b) {
-    
-    return a + b;
 }
